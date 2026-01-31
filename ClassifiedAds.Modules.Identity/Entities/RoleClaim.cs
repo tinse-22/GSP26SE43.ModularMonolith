cfc6a0d4c0cc4fc7431ce1f520601f0834707771
TinVT5 <tinvtse@gmail.com>
2026-01-31 23:36:18 +0700

    feat(identity): add UserProfile entity for custom user extension

--- ClassifiedAds.Modules.Identity/Entities/RoleClaim.cs
+++ ClassifiedAds.Modules.Identity/Entities/RoleClaim.cs
@@ -5,7 +5,10 @@ namespace ClassifiedAds.Modules.Identity.Entities;
 
 public class RoleClaim : Entity<Guid>
 {
+    public Guid RoleId { get; set; }
+
     public string Type { get; set; }
+
     public string Value { get; set; }
 
     public Role Role { get; set; }
--- ClassifiedAds.Modules.Identity/Entities/UserClaim.cs
+++ ClassifiedAds.Modules.Identity/Entities/UserClaim.cs
@@ -5,7 +5,10 @@ namespace ClassifiedAds.Modules.Identity.Entities;
 
 public class UserClaim : Entity<Guid>
 {
+    public Guid UserId { get; set; }
+
     public string Type { get; set; }
+
     public string Value { get; set; }
 
     public User User { get; set; }
