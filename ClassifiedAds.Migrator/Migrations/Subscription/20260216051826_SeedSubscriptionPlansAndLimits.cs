1329459326ada678fe8a8c1bf0d85f0e85857403
TinVT5 <tinvtse@gmail.com>
2026-02-16 12:22:31 +0700

    feat: seed subscription plans and limits data

---
+++ ClassifiedAds.Migrator/Migrations/Subscription/20260216051826_SeedSubscriptionPlansAndLimits.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ClassifiedAds.Migrator.Migrations.Subscription
+{
+    /// <inheritdoc />
+    public partial class SeedSubscriptionPlansAndLimits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "subscription",
+                table: "SubscriptionPlans",
+                columns: new[] { "Id", "CreatedDateTime", "Currency", "Description", "DisplayName", "IsActive", "Name", "PriceMonthly", "PriceYearly", "SortOrder", "UpdatedDateTime" },
+                values: new object[,]
+                {
+                    { new Guid("10000000-0000-0000-0000-000000000001"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "VND", "Basic plan for individuals getting started", "Free", true, "Free", 0m, 0m, 1, null },
+                    { new Guid("10000000-0000-0000-0000-000000000002"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "VND", "Professional plan for growing teams", "Professional", true, "Pro", 299000m, 2990000m, 2, null },
+                    { new Guid("10000000-0000-0000-0000-000000000003"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), "VND", "Enterprise plan for large organizations", "Enterprise", true, "Enterprise", 999000m, 9990000m, 3, null }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "subscription",
+                table: "PlanLimits",
+                columns: new[] { "Id", "CreatedDateTime", "IsUnlimited", "LimitType", "LimitValue", "PlanId", "UpdatedDateTime" },
+                values: new object[,]
+                {
+                    { new Guid("20000000-0000-0000-0000-000000000001"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 0, 1, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000002"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 1, 10, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000003"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 2, 20, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000004"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 3, 50, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000005"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 4, 1, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000006"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 5, 7, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000007"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 6, 10, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000008"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 7, 100, new Guid("10000000-0000-0000-0000-000000000001"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000011"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 0, 10, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000012"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 1, 50, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000013"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 2, 100, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000014"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 3, 500, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000015"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 4, 3, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000016"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 5, 30, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000017"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 6, 100, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000018"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 7, 1000, new Guid("10000000-0000-0000-0000-000000000002"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000021"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), true, 0, null, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000022"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), true, 1, null, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000023"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), true, 2, null, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000024"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), true, 3, null, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000025"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 4, 10, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000026"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), false, 5, 365, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000027"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), true, 6, null, new Guid("10000000-0000-0000-0000-000000000003"), null },
+                    { new Guid("20000000-0000-0000-0000-000000000028"), new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)), true, 7, null, new Guid("10000000-0000-0000-0000-000000000003"), null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000003"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000004"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000005"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000006"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000007"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000008"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000011"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000012"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000013"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000014"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000015"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000016"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000017"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000018"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000021"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000022"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000023"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000024"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000025"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000026"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000027"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "PlanLimits",
+                keyColumn: "Id",
+                keyValue: new Guid("20000000-0000-0000-0000-000000000028"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "SubscriptionPlans",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "SubscriptionPlans",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.DeleteData(
+                schema: "subscription",
+                table: "SubscriptionPlans",
+                keyColumn: "Id",
+                keyValue: new Guid("10000000-0000-0000-0000-000000000003"));
+        }
+    }
+}
--- ClassifiedAds.Modules.Subscription/DbConfigurations/PlanLimitConfiguration.cs
+++ ClassifiedAds.Modules.Subscription/DbConfigurations/PlanLimitConfiguration.cs
@@ -1,11 +1,19 @@
+using ClassifiedAds.Contracts.Subscription.Enums;
 using ClassifiedAds.Modules.Subscription.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 
 namespace ClassifiedAds.Modules.Subscription.DbConfigurations;
 
 public class PlanLimitConfiguration : IEntityTypeConfiguration<PlanLimit>
 {
+    private static readonly DateTimeOffset Epoch = new(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), TimeSpan.Zero);
+
+    private static readonly Guid FreePlanId = Guid.Parse("10000000-0000-0000-0000-000000000001");
+    private static readonly Guid ProPlanId = Guid.Parse("10000000-0000-0000-0000-000000000002");
+    private static readonly Guid EnterprisePlanId = Guid.Parse("10000000-0000-0000-0000-000000000003");
+
     public void Configure(EntityTypeBuilder<PlanLimit> builder)
     {
         builder.ToTable("PlanLimits");
@@ -18,5 +26,36 @@ public class PlanLimitConfiguration : IEntityTypeConfiguration<PlanLimit>
             .WithMany()
             .HasForeignKey(x => x.PlanId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasData(
+            // ── Free Plan Limits ──
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000001"), PlanId = FreePlanId, LimitType = LimitType.MaxProjects, LimitValue = 1, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000002"), PlanId = FreePlanId, LimitType = LimitType.MaxEndpointsPerProject, LimitValue = 10, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000003"), PlanId = FreePlanId, LimitType = LimitType.MaxTestCasesPerSuite, LimitValue = 20, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000004"), PlanId = FreePlanId, LimitType = LimitType.MaxTestRunsPerMonth, LimitValue = 50, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000005"), PlanId = FreePlanId, LimitType = LimitType.MaxConcurrentRuns, LimitValue = 1, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000006"), PlanId = FreePlanId, LimitType = LimitType.RetentionDays, LimitValue = 7, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000007"), PlanId = FreePlanId, LimitType = LimitType.MaxLlmCallsPerMonth, LimitValue = 10, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000008"), PlanId = FreePlanId, LimitType = LimitType.MaxStorageMB, LimitValue = 100, IsUnlimited = false, CreatedDateTime = Epoch },
+
+            // ── Pro Plan Limits ──
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000011"), PlanId = ProPlanId, LimitType = LimitType.MaxProjects, LimitValue = 10, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000012"), PlanId = ProPlanId, LimitType = LimitType.MaxEndpointsPerProject, LimitValue = 50, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000013"), PlanId = ProPlanId, LimitType = LimitType.MaxTestCasesPerSuite, LimitValue = 100, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000014"), PlanId = ProPlanId, LimitType = LimitType.MaxTestRunsPerMonth, LimitValue = 500, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000015"), PlanId = ProPlanId, LimitType = LimitType.MaxConcurrentRuns, LimitValue = 3, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000016"), PlanId = ProPlanId, LimitType = LimitType.RetentionDays, LimitValue = 30, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000017"), PlanId = ProPlanId, LimitType = LimitType.MaxLlmCallsPerMonth, LimitValue = 100, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000018"), PlanId = ProPlanId, LimitType = LimitType.MaxStorageMB, LimitValue = 1000, IsUnlimited = false, CreatedDateTime = Epoch },
+
+            // ── Enterprise Plan Limits ──
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000021"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxProjects, LimitValue = null, IsUnlimited = true, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000022"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxEndpointsPerProject, LimitValue = null, IsUnlimited = true, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000023"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxTestCasesPerSuite, LimitValue = null, IsUnlimited = true, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000024"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxTestRunsPerMonth, LimitValue = null, IsUnlimited = true, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000025"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxConcurrentRuns, LimitValue = 10, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000026"), PlanId = EnterprisePlanId, LimitType = LimitType.RetentionDays, LimitValue = 365, IsUnlimited = false, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000027"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxLlmCallsPerMonth, LimitValue = null, IsUnlimited = true, CreatedDateTime = Epoch },
+            new PlanLimit { Id = Guid.Parse("20000000-0000-0000-0000-000000000028"), PlanId = EnterprisePlanId, LimitType = LimitType.MaxStorageMB, LimitValue = null, IsUnlimited = true, CreatedDateTime = Epoch });
     }
 }
