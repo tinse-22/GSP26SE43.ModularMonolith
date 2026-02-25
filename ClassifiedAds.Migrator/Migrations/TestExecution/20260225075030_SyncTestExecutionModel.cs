dbe3d3925b1ad27af678f899d9b746b5f2e4b18b
TinVT5 <tinvtse@gmail.com>
2026-02-25 16:23:45 +0700

    chore(migrations): add TestExecution and TestGeneration database migrations

---
+++ ClassifiedAds.Migrator/Migrations/TestExecution/20260225075030_SyncTestExecutionModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClassifiedAds.Migrator.Migrations.TestExecution
+{
+    /// <inheritdoc />
+    public partial class SyncTestExecutionModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                schema: "testexecution",
+                table: "TestRuns",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                schema: "testexecution",
+                table: "TestRuns",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
